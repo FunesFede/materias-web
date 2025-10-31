797f458caa8f40a59c299f3dcf28adc58e8a3c79
Fede <204039362+FunesFede@users.noreply.github.com>
2025-10-30 23:33:52 -0300

    Edit off canvas name

--- src/App.jsx
+++ src/App.jsx
@@ -130,9 +130,9 @@ function App() {
 				</AsignaturasContext.Provider>
 			</UserStateContext.Provider>
 
-			<div className='offcanvas offcanvas-start' tabIndex='-1' id='offcanvasExample' aria-labelledby='offcanvasExampleLabel'>
+			<div className='offcanvas offcanvas-start' tabIndex='-1' id='GuiaBotones' aria-labelledby='GuiaBotones'>
 				<div className='offcanvas-header'>
-					<h5 className='offcanvas-title' id='offcanvasExampleLabel'>
+					<h5 className='offcanvas-title' id='offcanvasGuiaBotonesLabel'>
 						Guía Botones e Iconos
 					</h5>
 					<button type='button' className='btn-close' data-bs-dismiss='offcanvas' aria-label='Close'></button>
