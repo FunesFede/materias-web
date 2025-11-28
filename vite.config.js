07f3b15b265a3293d04cafd3ed3b49394094c31c
Fede <204039362+FunesFede@users.noreply.github.com>
2025-11-28 18:58:16 -0300

    Expose env variables

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
 	build: {
 		outDir: "dist",
 	},
+	define: {
+		"import.meta.env.VITE_CF_PAGES_COMMIT_SHA": JSON.stringify(process.env.CF_PAGES_COMMIT_SHA),
+		"import.meta.env.VITE_CF_PAGES_BRANCH": JSON.stringify(process.env.CF_PAGES_BRANCH),
+	},
 });
